59ef516b9adb162e1942383ddb0eafe96b5a96bb
Tal Ater <tal@talater.com>
2017-12-25 16:16:04 +0200

    Added precommit hook

---
+++ .pre-commit.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+git diff --staged --diff-filter=dx --name-only HEAD | grep ".*\.js$" | xargs -I % sh -c './node_modules/.bin/prettier --write --trailing-comma all --single-quote %; git add %'
+
+STAGED_FILES=$(git diff --staged --diff-filter=dx --name-only HEAD | grep ".*\.js$")
+
+PASS=true
+
+for FILE in $STAGED_FILES
+do
+  ./node_modules/.bin/eslint "$FILE"
+  if [[ "$?" != 0 ]]; then
+    PASS=false
+  fi
+done
+
+if ! $PASS; then
+  echo "\033[41mCOMMIT FAILED:\033[0m Your commit contains files that should pass ESLint but do not. Please fix the ESLint errors and try again.\n"
+  exit 1
+fi
