ad6f628dba716d2c53c7f82af47f07d5eec056fd
Tal Ater <tal@talater.com>
2017-12-27 00:08:10 +0200

    Use web3 to get real blocks from local blockchain

--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const Blocks = ({blocks}) => (
   <div>
     {blocks.map(block => (
-      <div key={block.id}>{block.id} - {block.transactions}</div>
+      <div key={block.hash}>{block.number} - {block.hash}</div>
     ))}
   </div>
 );
--- src/lib/blockchain.js
+++ src/lib/blockchain.js
@@ -1,3 +1,23 @@
+import Web3 from 'web3';
+const port = 8545;
+const web3 = new Web3(new Web3.providers.HttpProvider(`http://localhost:${port}`));
+
+export const getAccounts = () => web3.eth.getAccounts();
+
 export const getBlocks = () => {
-  return Promise.resolve([{ id: 3, transactions: 4 }, { id: 2, transactions: 3 }, { id: 1, transactions: 1 }]);
+  const maxBlockCount = 10;
+  // get block count
+  return web3.eth
+    .getBlockNumber()
+    .then(latestBlock => {
+      let blocks = [];
+      let nextBlock = latestBlock;
+      // fetch last blocks
+      while (nextBlock >= 0 && blocks.length < maxBlockCount) {
+        blocks.push(web3.eth.getBlock(nextBlock));
+        nextBlock--;
+      }
+      return Promise.all(blocks);
+    })
+    .then(blocks => Promise.resolve(blocks));
 };
