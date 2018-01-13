02f80b303c1258f62ff1d1dd1ab9b395eafef36e
shekit <as7790@nyu.edu>
2018-01-12 23:06:57 -0500

    added top blocks to reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import accounts from './accounts';
 import blocks from './blocks';
+import topBlocks from './topBlocks';
 
 export default combineReducers({
   accounts,
   blocks,
+  topBlocks
 });
