6c9f0aeef5b2b60881bdcdc0176c27bc8d8752ca
shekit <as7790@nyu.edu>
2018-01-13 18:27:58 -0500

    corrected div structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,20 +16,19 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div className="container xplorer">
-          <div className="row">
-            <div className="col-sm-6">
-              <a href="#" className="logo"><img src="../static/images/logo.png" /></a>
-            </div>
-            <div className="col-sm-6">
-              <SearchFormContainer />
-            </div>
+      <div className="container xplorer">
+        <div className="row">
+          <div className="col-sm-6">
+            <a href="#" className="logo"><img src="../static/images/logo.png" /></a>
+          </div>
+          <div className="col-sm-6">
+            <SearchFormContainer />
           </div>
         </div>
-        <div>
-          <h1>DAV Block Explorer</h1>
-        </div>
+
+
+        <h1>DAV Block Explorer</h1>
+
         <div className="gray-block">
           <TopBlocksContainer />
           <BlocksContainer />
