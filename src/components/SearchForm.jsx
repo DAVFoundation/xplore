00a6d81adb7b7b939d14017efba0292a7767411f
shekit <as7790@nyu.edu>
2018-01-12 22:27:02 -0500

    search form component

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Field} from 'redux-form';
+
+class SearchForm extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  submit(values){
+    console.log("values");
+    return;
+    //this.props.submitStationForm({values});
+  }
+
+  render(){
+    return (
+      <div className="row">
+        <form onSubmit={this.props.handleSubmit(this.submit)}>
+          <div className="col-xs-9">
+            <Field name="search" placeholder="Search by Tx Hash / Block # / Address" component="input"/>
+          </div>
+          <div className="col-xs-3">
+            <button type="submit">SEARCH</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+SearchForm.propTypes = {
+  handleSubmit: PropTypes.func,
+  submitStationForm: PropTypes.func
+};
+
+export default SearchForm;
