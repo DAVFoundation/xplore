b5a9c6a38ac90e4ff0122dfbe6d1606f7bfd6d64
shekit <as7790@nyu.edu>
2018-01-14 04:16:09 -0500

    fixed block date

--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -1,23 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const Blocks = ({blocks}) => (
-  <div className="col-sm-6">
-    <h3 className="block-title">Block</h3>
-    <div className="white-container">
-      <div className="row">
-        <div className="col-xs-4 text-left">
-          <p className="header-custom">Block #</p>
-        </div>
-        <div className="col-xs-4 text-center">
-          <p className="header-custom">Transactions</p>
-        </div>
-        <div className="col-xs-4 text-right">
-          <p className="header-custom">Time</p>
-        </div>
-      </div>
-      <div className="header-separator"></div>
-      {blocks.map((block,index) => (
+class Blocks extends Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+    let blocks = this.props.blocks.map((block, index) => {
+      let date = new Date(block.timestamp * 1000);
+      let dateOptions = {hour: '2-digit', minute: '2-digit',second:'2-digit', hour12: false};
+      return (
         <div className="row" key={index}>
           <div className="col-xs-4 text-left">
             <p className="block-number">{block.number}</p>
@@ -26,13 +20,35 @@ const Blocks = ({blocks}) => (
             <p>{block.transactions.length}</p>
           </div>
           <div className="col-xs-4 text-right">
-            <p>{block.timestamp}</p>
+            <p>{date.toLocaleString('en-US', dateOptions)}</p>
           </div>
         </div>
-      ))}
-    </div>
-  </div>
-);
+      );
+    });
+
+    return (
+      <div className="col-sm-6">
+        <h3 className="block-title">Block</h3>
+        <div className="white-container">
+          <div className="row">
+            <div className="col-xs-4 text-left">
+              <p className="header-custom">Block #</p>
+            </div>
+            <div className="col-xs-4 text-center">
+              <p className="header-custom">Transactions</p>
+            </div>
+            <div className="col-xs-4 text-right">
+              <p className="header-custom">Time</p>
+            </div>
+          </div>
+          <div className="header-separator"></div>
+          {blocks}
+        </div>
+      </div>
+    );
+  }
+}
+
 
 Blocks.propTypes = {
   blocks: PropTypes.array.isRequired,
