dfb87e1d9b5a9a26a0f65ae3f2a8d3646110b232
shekit <as7790@nyu.edu>
2018-01-14 04:22:33 -0500

    top block design edits

--- src/components/TopBlocks.jsx
+++ src/components/TopBlocks.jsx
@@ -29,11 +29,19 @@ class BlockItem extends Component {
   }
 
   render(){
+    let imgPath = `../static/images/${this.props.icon}`;
     return(
       <div className="col-sm-3">
         <div className={this.props.classId}>
-          <h4>{this.props.title}</h4>
-          <h3>{this.props.icon}</h3>
+          <div className="row">
+            <div className="col-xs-8">
+              <h4>{this.props.title}</h4>
+              <h3></h3>
+            </div>
+            <div className="col-xs-4">
+              <img src={imgPath}/>
+            </div>
+          </div>
         </div>
       </div>
     );
