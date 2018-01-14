86556225ee53e62e28ed3ff29c71284f3617d9ef
shekit <as7790@nyu.edu>
2018-01-14 04:29:38 -0500

    fixed search form styling

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -18,7 +18,7 @@ class SearchForm extends Component{
       <div className="row">
         <form onSubmit={this.props.handleSubmit(this.submit)}>
           <div className="col-xs-9">
-            <Field name="search" placeholder="Search by Tx Hash / Block # / Address" component="input"/>
+            <Field name="search" placeholder="Search by Tx Hash / Block # / Address" component="input" type="text"/>
           </div>
           <div className="col-xs-3">
             <button type="submit">SEARCH</button>
