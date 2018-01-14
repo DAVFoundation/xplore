94f0c5a49fb6f9616634d251cc32ce166ce82dd2
shekit <as7790@nyu.edu>
2018-01-13 22:53:32 -0500

    added styling to blocks

--- src/components/Accounts.jsx
+++ src/components/Accounts.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Blocks = ({accounts}) => (
-  <div>
-    <h1>Accounts:</h1>
-    {accounts.map(account => (
-      <div key={account}>{account}</div>
-    ))}
+  <div className="col-sm-6">
+    <h3 className="block-title">Accounts</h3>
+    <div className="white-container">
+      <div className="row">
+        <div className="col-xs-6 text-left">
+          <p className="header-custom">Address</p>
+        </div>
+        <div className="col-xs-6 text-right">
+          <p className="header-custom">Balance</p>
+        </div>
+      </div>
+
+      <div className="header-separator"></div>
+      {accounts.map(account => (
+        <div key={account} className="address-number">{account}</div>
+      ))}
+    </div>
   </div>
 );
 
--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Blocks = ({blocks}) => (
-  <div>
-    <h1>Latest Blocks:</h1>
-    {blocks.map(block => (
-      <div key={block.hash}>{block.number} - {block.hash}</div>
-    ))}
+  <div className="col-sm-6">
+    <h3 className="block-title">Block</h3>
+    <div className="white-container">
+      <div className="row">
+        <div className="col-xs-4 text-left">
+          <p className="header-custom">Block #</p>
+        </div>
+        <div className="col-xs-4 text-center">
+          <p className="header-custom">Transactions</p>
+        </div>
+        <div className="col-xs-4 text-right">
+          <p className="header-custom">Time</p>
+        </div>
+      </div>
+      <div className="header-separator"></div>
+      {blocks.map(block => (
+        <div key={block.hash}>{block.number} - {block.hash}</div>
+      ))}
+    </div>
   </div>
 );
 
