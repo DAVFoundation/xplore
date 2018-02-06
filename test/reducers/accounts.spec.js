c317dd317c1ad259b0808c57c613b4f59e1e2d2b
Tal Ater <tal@talater.com>
2018-02-07 00:09:26 +0200

    Renamed __tests__ to test

--- test/reducers/accounts.spec.js
+++ test/reducers/accounts.spec.js
@@ -6,9 +6,11 @@ describe('Accounts reducer', () => {
       const state = [];
       const action = {
         type: 'GET_ACCOUNTS_FULFILLED',
-        payload: [{ address: '0x080c96ed2700E238e3784B0B2B33e6B5f2CfD9e2', balance: '100' }]
+        payload: [{ address: '0x080c96ed2700E238e3784B0B2B33e6B5f2CfD9e2', balance: '100' }],
       };
-      expect(reducer(state, action)).toEqual([{ address: '0x080c96ed2700E238e3784B0B2B33e6B5f2CfD9e2', balance: '100' }]);
+      expect(reducer(state, action)).toEqual([
+        { address: '0x080c96ed2700E238e3784B0B2B33e6B5f2CfD9e2', balance: '100' },
+      ]);
     });
   });
 });
