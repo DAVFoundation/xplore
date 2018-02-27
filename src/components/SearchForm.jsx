d6e78c31524b19a2d25026b95d3747d849b55682
addieconst <constanteadi@gmail.com>
2018-02-26 16:55:45 -0800

    Fix for issue #61

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Field} from 'redux-form';
 
 class SearchForm extends Component{
   constructor(props){
@@ -20,11 +19,12 @@ class SearchForm extends Component{
   }
 
   render(){
+    console.log(this.props);
     return (
       <div className="row">
-        <form onSubmit={this.props.handleSubmit(this.submit)}>
+        <form onSubmit={this.submit}>
           <div className="col-xs-9">
-            <Field name="search" placeholder="Search by Tx Hash / Block # / Address" component="input" type="text" onChange={this.handleChange} value={this.state.value} />
+            <input name="search" placeholder="Search by Tx Hash / Block # / Address" component="input" type="text" onChange={this.handleChange} value={this.state.value} />
           </div>
           <div className="col-xs-3">
             <button type="submit">SEARCH</button>
